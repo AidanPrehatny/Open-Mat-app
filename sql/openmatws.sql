e4d7761e66277ba2831cb225ec234e5b3d703538
Nick Imkamp <imkampn@gmail.com>
2018-12-19 07:24:11 -0800

    Change data type of id to handle hash. Add columns for latitude and longitude

--- sql/openmatws.sql
+++ sql/openmatws.sql
@@ -1,7 +1,9 @@
 CREATE TABLE openmatdata(
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     days_hours CHAR(50) NOT NULL,
     street CHAR(50) NOT NULL,
-    phone CHAR(50) NOT NULL
+    phone CHAR(50) NOT NULL,
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL
 );
