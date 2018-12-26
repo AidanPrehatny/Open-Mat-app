33b3a7f33f89a71889421c0f8bec96a8358a3d51
Nick Imkamp <imkampn@gmail.com>
2018-12-26 14:12:04 -0800

    Change datatype of id to INT PRIMARY KEY to handle numeric id numbers. Change datatype of street to TEXT to handle larger character count. or your changes. Lines starting

--- sql/openmatws.sql
+++ sql/openmatws.sql
@@ -1,8 +1,8 @@
 CREATE TABLE openmatdata(
-    id SERIAL PRIMARY KEY,
+    id INT PRIMARY KEY,
     name TEXT NOT NULL,
     days_hours CHAR(50) NOT NULL,
-    street CHAR(50) NOT NULL,
+    street TEXT NOT NULL,
     phone CHAR(50) NOT NULL,
     latitude FLOAT NOT NULL,
     longitude FLOAT NOT NULL
